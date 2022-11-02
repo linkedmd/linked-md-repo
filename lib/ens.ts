b25941fa3e1fcf36699edfd051285c80fbc72048
Luis Cuende <me@luisivan.net>
2022-11-02 11:04:50 +0000

    fix(): Fixing Alchemy (https://github.com/rainbow-me/rainbowkit/issues/843#issuecomment-1290981567)

--- lib/ens.ts
+++ lib/ens.ts
@@ -6,7 +6,7 @@ const ETH_ADDRESS_REGEX = /^0x[a-fA-F0-9]{40}$/
 const DOMAIN_REGEX =
   /^(((?!\-))(xn\-\-)?[a-z0-9\-_]{0,61}[a-z0-9]{1,1}\.)*(xn\-\-)?([a-z0-9\-]{1,61}|[a-z0-9\-]{1,30})\.[a-z]{2,}$/
 
-const provider = providers.WebSocketProvider(
+const provider = ethers.providers.WebSocketProvider(
   `wss://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_ID}`,
   chain.mainnet.id
 )
